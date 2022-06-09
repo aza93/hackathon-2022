15fa93625105867558e77ce1d07d5e9845edda3e
Abdellah Azabar <a.azabar93@gmail.com>
2022-06-10 01:54:54 +0200

    Popups + books openers (floor 0 map, json, ...)

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,65 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+import books from './books.json';
+import popups from './popups.json';
 
-let currentPopup: any = undefined;
+console.log("Script started successfully");
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
+    // Clock popup
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    popupOperations("clockZone", "clockPopup", "It's " + time)
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+    // Books categories popups
+    for (let i = 0; i < popups.length; i += 3) {
+        let array = popups.slice(i, i+3);
+
+        popupOperations(array[0], array[1], array[2]);
+    }
+
+    // Books websites opener
+    openWebsites(books);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
-}).catch(e => console.error(e));
+function popupOperations(
+  zoneName: string,
+  popupName: string,
+  popupMessage: string
+) {
+  let popupObject: any = undefined;
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+  WA.room.onEnterLayer(zoneName).subscribe(() => {
+    popupObject = WA.ui.openPopup(popupName, popupMessage, []);
+  });
+
+  // Popup closing
+  WA.room.onLeaveLayer(zoneName).subscribe(() => {
+      if (popupObject !== undefined) {
+        popupObject.close();
+        popupObject = undefined;
+      }
+  });
+}
+
+function openWebsites(books: any) {
+    Object.entries(books).forEach(([zoneName, link]) =>
+        WA.room.setProperty(zoneName, "openWebsite", link as string)
+    );
 }
 
 export {};
