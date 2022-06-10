d2e784e1dd6ee15c9f63d181b26efbc096357b15
t.johan14 <johan.travert@aliceadsl.fr>
2022-06-10 15:24:29 +0200

    change names of zones + remove wrong files

--- Outside_A3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Outside_A3" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <image source="../../../../../../../Users/intern/Downloads/LPC/Tilesets/Outside_A3.png" width="512" height="256"/>
-</tileset>
